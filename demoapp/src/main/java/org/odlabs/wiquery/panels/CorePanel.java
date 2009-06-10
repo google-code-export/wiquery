6d554a1c8910b91aeacb6176993622dad651eca3
pierre.marguerite <pierre.marguerite@21b45634-0984-11de-b025-799c3e7cca98>
2009-06-10 15:36:37 +0000

    added missing  serialVersionUID attribute

--- demoapp/src/main/java/org/odlabs/wiquery/panels/CorePanel.java
+++ demoapp/src/main/java/org/odlabs/wiquery/panels/CorePanel.java
@@ -12,28 +12,35 @@ import org.odlabs.wiquery.plugin.ChilliPanel;
 
 public class CorePanel extends Panel {
 
+	private static final long serialVersionUID = 1L;
+
 	public CorePanel(String id) {
 		super(id);
 		ChilliPanel chilliPlugin = new ChilliPanel("examples");
-		add(chilliPlugin);
+		this.add(chilliPlugin);
+
 		Label example1 = new Label("example1", "Click to run example 1");
 		example1.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
-		
+
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public JsScope callback() {
 				return new JsScope() {
-				
+
+					private static final long serialVersionUID = 1L;
+
 					@Override
 					protected void execute(JsScopeContext scopeContext) {
-						scopeContext.self().chain(CssHelper.css("border", "1px solid red"));
+						scopeContext.self().chain(
+								CssHelper.css("border", "1px solid red"));
 					}
-				
+
 				};
 			}
-		
+
 		}));
 		chilliPlugin.add(example1);
 	}
 
 }
-
--- demoapp/src/main/java/org/odlabs/wiquery/panels/DialogPanel.java
+++ demoapp/src/main/java/org/odlabs/wiquery/panels/DialogPanel.java
@@ -17,47 +17,54 @@ import org.odlabs.wiquery.ui.dialog.Dialog;
  */
 public class DialogPanel extends Panel {
 
+	private static final long serialVersionUID = 1L;
+
 	private AjaxContent newContent = null;
-	
+
 	public DialogPanel(String id) {
 		super(id);
 		ChilliPanel chilliPlugin = new ChilliPanel("examples");
-		add(chilliPlugin);
+		this.add(chilliPlugin);
 		Button button = new Button("openSimpleDialog");
 		chilliPlugin.add(button);
 		final Dialog dialog = new Dialog("simpleDialog");
 		button.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
-		
+
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public JsScope callback() {
 				return JsScope.quickScope(dialog.open().render());
 			}
-		
+
 		}));
 		chilliPlugin.add(dialog);
-		
+
 		button = new Button("openAjaxDialog");
 		chilliPlugin.add(button);
 		final Dialog ajaxDialog = new Dialog("ajaxDialog");
-		final WebMarkupContainer container = new WebMarkupContainer("ajaxContent");
+		final WebMarkupContainer container = new WebMarkupContainer(
+				"ajaxContent");
 		container.setOutputMarkupId(true);
 		ajaxDialog.add(container);
 		button.add(new WiQueryAjaxEventBehavior(MouseEvent.CLICK) {
-		
+
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			protected void onEvent(AjaxRequestTarget target) {
-				if (newContent == null) {
-					newContent = new AjaxContent("ajaxContent");
-					newContent.setOutputMarkupId(true);
-					container.replaceWith(newContent);
-					target.addComponent(newContent, container.getMarkupId());
+				if (DialogPanel.this.newContent == null) {
+					DialogPanel.this.newContent = new AjaxContent("ajaxContent");
+					DialogPanel.this.newContent.setOutputMarkupId(true);
+					container.replaceWith(DialogPanel.this.newContent);
+					target.addComponent(DialogPanel.this.newContent, container
+							.getMarkupId());
 				}
 				ajaxDialog.open(target);
 			}
-		
+
 		});
 		chilliPlugin.add(ajaxDialog);
 	}
 
 }
-
