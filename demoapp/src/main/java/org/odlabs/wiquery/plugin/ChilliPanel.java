b8c6cba1006ba4d1fedd458874f28807471a7dc2
pierre.marguerite <pierre.marguerite@21b45634-0984-11de-b025-799c3e7cca98>
2009-06-11 07:44:23 +0000

    added missing serialVersionUID attribute

--- demoapp/src/main/java/org/odlabs/wiquery/plugin/ChilliPanel.java
+++ demoapp/src/main/java/org/odlabs/wiquery/plugin/ChilliPanel.java
@@ -8,13 +8,17 @@ import org.odlabs.wiquery.core.javascript.JsStatement;
 
 public class ChilliPanel extends WebMarkupContainer implements IWiQueryPlugin {
 
+	private static final long serialVersionUID = 1L;
+
 	public ChilliPanel(String id) {
 		super(id);
 	}
 
 	public void contribute(WiQueryResourceManager wiQueryResourceManager) {
-		wiQueryResourceManager.addJavaScriptResource(ChilliPanel.class, "jquery.chili-2.2.js");
-		wiQueryResourceManager.addJavaScriptResource(ChilliPanel.class, "recipes.js");
+		wiQueryResourceManager.addJavaScriptResource(ChilliPanel.class,
+				"jquery.chili-2.2.js");
+		wiQueryResourceManager.addJavaScriptResource(ChilliPanel.class,
+				"recipes.js");
 	}
 
 	public JsStatement statement() {
