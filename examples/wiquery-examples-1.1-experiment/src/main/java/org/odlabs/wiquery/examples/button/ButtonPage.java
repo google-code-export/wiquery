bbc7e08c83385b47c1b603068778d9601d6ff3df
roche.jul <roche.jul@21b45634-0984-11de-b025-799c3e7cca98>
2010-09-28 20:07:45 +0000

    Wiquery 1.1 & 1.1 examples
    -> issue 84

--- examples/wiquery-examples-1.1-experiment/src/main/java/org/odlabs/wiquery/examples/button/ButtonPage.java
+++ examples/wiquery-examples-1.1-experiment/src/main/java/org/odlabs/wiquery/examples/button/ButtonPage.java
@@ -6,10 +6,16 @@ import java.util.List;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
+import org.odlabs.wiquery.core.events.Event;
+import org.odlabs.wiquery.core.events.MouseEvent;
+import org.odlabs.wiquery.core.events.WiQueryEventBehavior;
+import org.odlabs.wiquery.core.javascript.JsScope;
 import org.odlabs.wiquery.examples.AbstractExamplePage;
-import org.odlabs.wiquery.ui.button.Button;
-import org.odlabs.wiquery.ui.button.ButtonAjax;
 import org.odlabs.wiquery.ui.button.ButtonBehavior;
 import org.odlabs.wiquery.ui.button.ButtonCheckSet;
 import org.odlabs.wiquery.ui.button.ButtonElement;
@@ -33,6 +39,9 @@ public class ButtonPage extends AbstractExamplePage {
 	public ButtonPage(final PageParameters parameters) {
 		super("Button example");
 		
+		Form<String> formButtons = new Form<String>("formButtons");
+		add(formButtons);
+		
 		// Behavior button
 		AjaxLink<String> wicketLink = new AjaxLink<String> ("wicketLink") {
 			private static final long serialVersionUID = 1L;
@@ -49,31 +58,48 @@ public class ButtonPage extends AbstractExamplePage {
 		ButtonBehavior buttonBehavior = new ButtonBehavior();
 		buttonBehavior.setLabel("An ajax link with wicket");
 		wicketLink.add(buttonBehavior);
-		add(wicketLink);
+		formButtons.add(wicketLink);
 		
 		// Classic buttons
-		add(new Button("firstButton", "A button element"));
-		add(new Button("secondButton"));
-		add(new Button("thirdButton", "An anchor"));
 		
-		Button fourthButton = new Button("fourthButton", "A label set via Wicket");
-		fourthButton.setIcons(new ButtonIcon("ui-icon-gear", "ui-icon-triangle-1-s"));
-		fourthButton.setClickEvent(JsScopeUiEvent.quickScope("alert('you click me');"));
-		add(fourthButton);
+		formButtons.add(new Button("firstButton").add(new ButtonBehavior().setLabel("A button element")));
+		formButtons.add(new Button("secondButton").add(new ButtonBehavior()));
+		formButtons.add(new Button("thirdButton").add(new ButtonBehavior().setLabel("An anchor")));
 		
-		// Ajax button
-		add(new ButtonAjax("fifthButton", "An ajax button") {
+		WebMarkupContainer fourthButton = new WebMarkupContainer("fourthButton");
+		ButtonBehavior buttonBehavior4 = new ButtonBehavior();
+		buttonBehavior4.setLabel("A label set via Wicket");
+		buttonBehavior4.setIcons(new ButtonIcon("ui-icon-gear", "ui-icon-triangle-1-s"));
+		fourthButton.add(buttonBehavior4);
+		fourthButton.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
 			private static final long serialVersionUID = 1L;
 
 			/**
 			 * {@inheritDoc}
-			 * @see org.odlabs.wiquery.ui.button.ButtonAjax#onClick(org.apache.wicket.ajax.AjaxRequestTarget)
+			 * @see org.odlabs.wiquery.core.events.Event#callback()
 			 */
 			@Override
-			public void onClick(AjaxRequestTarget target) {
+			public JsScope callback() {
+				return JsScopeUiEvent.quickScope("alert('you click me');");
+			}
+		}));
+		formButtons.add(fourthButton);
+		
+		// Ajax button
+		AjaxButton ajaxButton = new AjaxButton("fifthButton", formButtons) {
+			private static final long serialVersionUID = 1L;
+
+			/**
+			 * {@inheritDoc}
+			 * @see org.apache.wicket.ajax.markup.html.form.AjaxButton#onSubmit(org.apache.wicket.ajax.AjaxRequestTarget, org.apache.wicket.markup.html.form.Form)
+			 */
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
 				target.appendJavascript("alert('Ajax call !!');");
 			}
-		});
+		};
+		ajaxButton.add(new ButtonBehavior());
+		formButtons.add(ajaxButton);
 		
 		// Radio & check set
 		List<ButtonElement<String>> elements = new ArrayList<ButtonElement<String>>();
@@ -81,7 +107,7 @@ public class ButtonPage extends AbstractExamplePage {
 		elements.add(new ButtonElement<String>(new Model<String>("button2"), new Model<String>("Button Two")));
 		elements.add(new ButtonElement<String>(new Model<String>("button3"), new Model<String>("Button Three")));
 		
-		add(new ButtonRadioSet<String>("radioSet", elements));
-		add(new ButtonCheckSet<String>("checkSet", elements));
+		formButtons.add(new ButtonRadioSet<String>("radioSet", elements));
+		formButtons.add(new ButtonCheckSet<String>("checkSet", elements));
 	}
 }
