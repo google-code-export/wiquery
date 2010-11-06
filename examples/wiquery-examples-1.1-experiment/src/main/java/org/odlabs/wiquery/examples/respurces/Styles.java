c7663a763399d05ab0f9caccb23e3a74b80eb920
roche.jul <roche.jul@21b45634-0984-11de-b025-799c3e7cca98>
2010-11-06 16:32:43 +0000

    Cleaning the svn

--- examples/wiquery-examples-1.1-experiment/src/main/java/org/odlabs/wiquery/examples/respurces/Styles.java
+++
@@ -1,9 +0,0 @@
-package org.odlabs.wiquery.examples.respurces;
-
-import org.apache.wicket.ResourceReference;
-
-public class Styles {
-	
-	public static ResourceReference CSS_MAIN = new ResourceReference(Styles.class, "application.css");
-
-}
--- wiquery-plugins/trunk/readme.txt
+++
@@ -1,1 +0,0 @@
-Wiquery plugins
